dd38fa170a6aa0e428cdd246890e5f3b45aa472b
Ankit Saini <saiankit542005@gmail.com>
2026-01-01 14:40:36 +0530

    Add Intro.java

---
+++ ArrayList/Easy/Intro.java
@@ -0,0 +1,13 @@
+package ArrayList.Easy;
+
+import java.util.ArrayList;
+
+public class Intro {
+    public static void main(String[] args) {
+        // ClassName objectName = new ClassName()
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+        ArrayList<Float> list4 = new ArrayList<>();
+    }
+}
