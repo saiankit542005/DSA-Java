5c7304f9474ac3ce3cfb15319aa56d16dfad1019
Ankit Saini <saiankit542005@gmail.com>
2026-01-28 19:55:38 +0530

    Added JavaCollFrame.java

---
+++ queues/easy/JavaCollFrame.java
@@ -0,0 +1,17 @@
+package queues.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JavaCollFrame {
+    public static void main(String[] args) {
+       Queue<Integer> q = new LinkedList<>();
+       q.add(1);
+       q.add(2);
+       q.add(3);
+
+       while(!q.isEmpty()){
+        System.out.println(q.remove());
+       }
+    }
+}
