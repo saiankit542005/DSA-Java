8b4237bd0e8d6f62136f1c5cf0cf339a21fc6dc2
Ankit Saini <saiankit542005@gmail.com>
2026-02-17 23:23:56 +0530

    Added CountNodes.java

---
+++ binary_trees/easy/CountNode.java
@@ -0,0 +1,38 @@
+package binary_trees.easy;
+
+public class CountNode {
+        static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    public static int count(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lCount = count(root.left);
+        int rCount = count(root.right);
+
+        return lCount + rCount + 1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Total Nodes : " + count(root));
+    }
+}
