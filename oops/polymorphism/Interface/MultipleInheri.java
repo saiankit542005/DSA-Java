5a94f0bb4e99e6b2b37f231f0a752a100fe645ca
Ankit Saini <saiankit542005@gmail.com>
2026-02-17 23:47:26 +0530

    Added MultipleInheri.java

---
+++ oops/polymorphism/Interface/MultipleInheri.java
@@ -0,0 +1,19 @@
+package polymorphism.Interface;
+
+public class MultipleInheri {
+    public static void main(String[] args) {
+        
+    }
+}
+
+interface Herbivore {
+
+}
+
+interface Carnivore {
+
+}
+
+class Bear implements Herbivore,Carnivore {
+    
+}
