69daa0fddce188716722b0ddaad13b21ca65dfa7
Ankit Saini <saiankit542005@gmail.com>
2026-02-19 11:12:21 +0530

    Added BuildBST.java

---
+++ binary_search_trees/medium/BuildBST.java
@@ -0,0 +1,56 @@
+package binary_search_trees.medium;
+
+public class BuildBST {
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public TreeNode insert(TreeNode root, int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+            return root;
+        }
+        if (root.data > value) {
+            // left subtree
+            root.left = insert(root.left, value);
+        } else {
+            // rigth subtree
+            root.right = insert(root.right, value);
+        }
+
+        return root;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        // int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = { 2, 3, 5, 7, 8, 1, 6, 4, 9 };
+        TreeNode root = null;
+
+        BuildBST obj = new BuildBST();
+
+        for (int i = 0; i < values.length; i++) {
+            root = obj.insert(root, values[i]);
+        }
+        
+        //In BST, inorder traversal give sorted order of the tree.
+        System.out.println("==== Inorder Traversal ====");
+        obj.inorder(root);
+        System.out.println();
+
+    }
+}
